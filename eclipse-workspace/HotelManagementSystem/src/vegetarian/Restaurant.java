887f474ef88e490ad52974762e7738b28f9d0d32
Surajdhage31 <surajdhage2006@gmail.com>
2024-02-09 12:42:29 +0530

    Hotel menu card paid if you want

--- eclipse-workspace/HotelManagementSystem/src/vegetarian/Restaurant.java
+++ eclipse-workspace/HotelManagementSystem/src/vegetarian/Restaurant.java
@@ -1,112 +1,109 @@
 package vegetarian;
+
 import java.util.Scanner;
+
 public class Restaurant {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	void Hotel() {
 		Scanner menu = new Scanner(System.in);
-    System.out.println("Enter your choice");
-    System.out.println("1.Break-fast ,2.lunch,3.Dinner");
-    int choice = menu.nextInt();
-    switch(choice)
-    {
-    case 1 :
-    {
-    	 System.out.println("Menu");
-    	 System.out.println("Pleses oder your favourite item");
-    	  System.out.println("1.Tea");
-    	  System.out.println("2.coffee");
-    	  System.out.println("3.pohe");
-    	  System.out.println("4.vada_pav");
-    	  System.out.println("5.pav bhaji");
-    	  System.out.println("6.dosa");
-    	  int Menu = menu.nextInt();
-    	  switch (Menu)
-    	  {
-    	  case 1 :
-    	  {
-    		  System.out.println(" please paid for Tea Rs 10/per");
-    		  System.out.println("please enter the quantity");
-    		  int quantity = menu.nextInt();
-    		  int price = 10 ;
-    		  int Ruppes=price*quantity;
-    		  System.out.printf("please paid "+Ruppes);
-    		  int fee = menu.nextInt();
-    		  String massage =(Ruppes == fee)?"thank for visit \n visit again \npaid Rs : "+fee :"not match price \n please paid  "+fee; 
-    		
-    		  System.out.println(massage);
-    		  
-    		  break ;
-    	  }
-    	  case 2:
-    	  {
-    		  System.out.println(" please paid for Coffee Rs 15/per");
-    		  System.out.println("please enter the quantity");
-    		  int quantity = menu.nextInt();
-    		  int price = 15 ;
-    		  int Ruppes=price*quantity;
-    		  System.out.printf("please paid "+Ruppes);
-    		  int fee = menu.nextInt();
-    		  String massage =(Ruppes == fee)?"thank for visit \n visit again \npaid Rs : "+fee :"not match price \n please paid  "+fee; 
-    		  System.out.println(massage);
-    		  break ;
-    	  }
-    	  case 3:
-    	  {
-    		  System.out.println(" please paid for Pohe Rs 25/per");
-    		  System.out.println("please enter the quantity");
-    		  int quantity = menu.nextInt();
-    		  int price = 25 ;
-    		  int Ruppes=price*quantity;
-    		  System.out.printf("please paid "+Ruppes);
-    		  int fee = menu.nextInt();
-    		  String massage =(Ruppes == fee)?"thank for visit \n visit again \npaid Rs : "+fee :"not match price \n please paid  "+fee; 
-    		  System.out.println(massage); 
-    		  break ;
-    	  }
-    	  case 4:
-    	  {
-    		  System.out.println(" please paid for vada pav Rs 15/per");
-    		  System.out.println("please enter the quantity");
-    		  int quantity = menu.nextInt();
-    		  int price = 15 ;
-    		  int Ruppes=price*quantity;
-    		  System.out.printf("please paid "+Ruppes);
-    		  int fee = menu.nextInt();
-    		  String massage =(Ruppes == fee)?"thank for visit \n visit again \npaid Rs : "+fee :"not match price \n please paid  "+fee; 
-    		  System.out.println(massage);
-    		  break ;
-    	  }
-    	  case 5:
-    	  {
-    		  System.out.println(" please paid for Pav bhaji Rs 40/per");
-    		  System.out.println("please enter the quantity");
-    		  int quantity = menu.nextInt();
-    		  int price = 40;
-    		  int Ruppes=price*quantity;
-    		  System.out.printf("please paid "+Ruppes);
-    		  int fee = menu.nextInt();
-    		  String massage =(Ruppes == fee)?"thank for visit \n visit again \npaid Rs : "+fee :"not match price \n please paid  "+fee; 
-    		  System.out.println(massage);
-    		  break ;
-    	  }
-    	  case 6 :
-    	  {
-    		  System.out.println(" please paid for Dosa Rs 50/per");
-    		  System.out.println("please enter the quantity");
-    		  int quantity = menu.nextInt();
-    		  int price = 50 ;
-    		  int Ruppes=price*quantity;
-    		  System.out.printf("please paid "+Ruppes);
-    		  int fee = menu.nextInt();
-    		  String massage =(Ruppes == fee)?"thank for visit \n visit again \npaid Rs : "+fee :"not match price \n please paid  "+fee; 
-    		  System.out.println(massage);  
-    		  break ;
-    	  }
-    	  }
-    }
-    }
- 
-	}
+		System.out.println("Enter your choice");
+		System.out.println("1.Break-fast,2.Lunch,3.Dinner");
+		int choice = menu.nextInt();
+		switch (choice) {
+		case 1: {
+			System.out.println("Menu");
+			System.out.println("1.Tea");
+			System.out.println("2.Coffee");
+			System.out.println("3.Pohe");
+			System.out.println("4.Vada pav");
+			System.out.println("5.Pav bhaji");
+			System.out.println("6.Dosa");
+			System.out.println("Pleses order your favourite item\n");
+			int Menu = menu.nextInt();
+			switch (Menu) {
+			case 1: {
+				System.out.println(" Please pay for Tea Rs 10/per");
+				System.out.println("Please enter the quantity");
+				int quantity = menu.nextInt();
+				int price = 10;
+				int Total = price * quantity;
+				System.out.println("Please pay " + Total);
+				int money = menu.nextInt();
+				String Result = (Total == money) ? "Thank for visit \n Visit again \nPaid Rs : " + money
+						: "Not match price \n Please pay for " + quantity + " Rs : " + money;
 
+				System.out.println(Result);
+
+				break;
+			}
+			case 2: {
+				System.out.println(" Please pay for Coffee Rs 15/per");
+				System.out.println("Please enter the quantity");
+				int quantity = menu.nextInt();
+				int price = 15;
+				int Total = price * quantity;
+				System.out.println("Please pay " + Total);
+				int money = menu.nextInt();
+				String Result = (Total == money) ? "Thank for visit \n Visit again \nPaid Rs : " + money
+						: "Not match price \n Please pay for " + quantity + " Rs : " + money;
+				System.out.println(Result);
+				break;
+			}
+			case 3: {
+				System.out.println("Please pay for Pohe Rs 25/per");
+				System.out.println("Please enter the quantity");
+				int quantity = menu.nextInt();
+				int price = 25;
+				int Total = price * quantity;
+				System.out.println("Please pay " + Total);
+				int money = menu.nextInt();
+				String Result = (Total == money) ? "Thank for visit \n Visit again \n Paid Rs:" + money
+						: "Not match price \n Please pay for " + quantity + " Rs : " + money;
+				System.out.println(Result);
+				break;
+			}
+			case 4: {
+				System.out.println("Please pay for Vada pav Rs 15/per");
+				System.out.println("Please enter the quantity");
+				int quantity = menu.nextInt();
+				int price = 15;
+				int Total = price * quantity;
+				System.out.println("Please pay " + Total);
+				int money = menu.nextInt();
+				String Result = (Total == money) ? "Thank for visit \n Visit again \nPaid Rs : " + money
+						: "Not match price \n Please pay for " + quantity + " Rs :" + money;
+				System.out.println(Result);
+				break;
+			}
+			case 5: {
+				System.out.println(" Please pay for Pav bhaji Rs 40/per");
+				System.out.println("Please enter the quantity");
+				int quantity = menu.nextInt();
+				int price = 40;
+				int Total = price * quantity;
+				System.out.println("Please pay " + Total);
+				int money  = menu.nextInt();
+				String Result = (Total == money) ? "Thank for visit \n Visit again \nPaid Rs : " + money
+						: "Not match price \n Please pay for " + quantity + " Rs : " + money;
+				System.out.println(Result);
+				break;
+			}
+			case 6: {
+				System.out.println(" Please pay for Dosa Rs 50/per");
+				System.out.println("Please enter the quantity");
+				int quantity = menu.nextInt();
+				int price = 50;
+				int Total = price * quantity;
+				System.out.println("Please pay " + Total);
+				int money = menu.nextInt();
+				String Result = (Total == money) ? "Thank for visit \n Visit again \nPaid Rs : " + money
+						: "Not match price \n Please pay for " + quantity + " Rs : " + money;
+				System.out.println(Result);
+				break;
+			}
+			}
+		}
+		}
+
+	}
 }
---
+++ eclipse-workspace/HotelManagementSystem/src/vegetarian/Tester.java
@@ -0,0 +1,11 @@
+package vegetarian;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Restaurant item = new Restaurant ();
+		item.Hotel();
+
+	}
+
+}
